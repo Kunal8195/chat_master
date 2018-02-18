a1cc23b8b8a20fce8b72e67e17044e3ac9e0dc0e
Kunal8195 <kpal9518@gmail.com>
2018-02-18 23:05:52 +0530

    ajax request working

--- client/index.js
+++ client/index.js
@@ -1,51 +1,14 @@
-'use strict'
-//const {Client} = require('@xmpp/client')
-
-//var name = $get('#fullname').text()
-//console.log(name);
-
-
-
-  const {xmpp, xml} =
-    typeof require === 'undefined' ? window.xmpp : require('..') // For you; require('@xmpp/client')
-
-//const {client} = xmpp()
-
-
-const client = new Client()
-//const xml = require('@xmpp/xml')
-console.log("connectio restarting")
-client.start('xmpp://localhost:5222').catch(err => {
-    console.error('start failed', err)
+  
+$.ajax({
+  type: "POST",
+  url: "http://localhost:3000/api/createUser",
+  ContentType:'application/json',
+  dataType:'json',
+  data:{
+    username:'amitabh',
+    password:'amitabh@123'
+  },
+  success: function(data){
+      console.log('succes: '+data);
+  }
 })
-
-client.handle('authenticate', authenticate => {
-  return authenticate('gabbar', 'gabbar@123')
-})
-
-client.on('error', err => {
-  console.error('❌', err.toString())
-})
-
-client.on('status', (status, value) => {
-  console.log('🛈', status, value ? value.toString() : '')
-})
-
-client.on('online', jid => {
-  console.log('🗸', 'online as', jid.toString())
-  client.emit('stanza',{
-	to:'admin@localhost',
-	text:'hello how u doing ?'
-});
-  //client.emit(xml('message'))
-})
-
-client.on('stanza', stanza => {
-  console.log('⮈', stanza.text);
-})
-
-
- 
-//const xml = require('@xmpp/xml')
-
-
--- server/server.js
+++ server/server.js
@@ -1,27 +1,85 @@
-const {Client} = require('@xmpp/client')
-
-const client = new Client()
-
-client.start('xmpp://localhost:5222').catch(err => {
-    console.error('start failed', err)
+/*$.get('/api/Activity/getAllActivity',function(data){
+	console.log(data);
 })
 
-client.on('error', err => {
-  console.error('❌', err.toString())
+$.ajax({
+  method: "get",
+  url: "http://localhost:8004/api/Activity/getAllActivity",
+  headers:{
+  	'authorization' : 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjVhNTViNTg4Y2RmYjZmMWY2MmYxMmY1YiIsInR5cGUiOiJVU0VSIiwiaWF0IjoxNTE1NTY2NDcyfQ.ifz68NJW7ZoAeWu594XU7qDauWhW5Ke9whDvID9asUw',
+  	'Content-Type':'application/json'
+  },
+  dataType:'json',
+  success: function(data){
+      console.log('succes: '+data);
+  }
 })
+  //.done(function( msg ) {
+   // alert( "Data Saved: " + msg );
+  });*/
 
-client.on('status', (status, value) => {
-  console.log('🛈', status, value ? value.toString() : '')
-})
+var Hapi = require('hapi');
+var requestPromise = require('minimal-request-promise');
+var Joi = require('joi');
 
-client.on('online', jid => {
-  console.log('🗸', 'online as', jid.toString())
-})
+//var Routes = require('');
+var server = new Hapi.Server({
+    app: {
+        name: 'Chat_App'
+    }
+});
+server.connection({
+    port: 3000,
+    routes: { cors: true }
+});
+/*server.route(
+    {
+        method: 'GET',
+        path: '/',
+        handler: function (req, reply) {
+            //TODO Change for production server
+            reply('<h1>hello</h1>')
+        }
+    }
+);*/
 
-client.on('stanza', function(stanza){
-    console.log(stanza);
-})
+server.route(
+{
+  method:'POST',
+  path: '/api/createUser',
+  handler: function (req, reply){
+    console.log(req.payload);
+    objToSave = {
+      username: req.payload.username,
+      password: req.payload.password
+    }
+    options = {
+        headers: {
+            'Authorization': 'Oj8svm7aVnfz5wng',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(objToSave)
+    };
+  requestPromise.post('http://localhost:9090/plugins/restapi/v1/users', options)
+        .then(function(response) {
+            reply(response);
+        })
+        .catch(function(error) {
+            // Deal with the error
+            console.log(options);
+            console.log(error);
+            reply(error);
+        }) 
+   }
+}        
+);
+//server.route(Routes);
+server.start((err) => {
 
-client.handle('authenticate', authenticate => {
-  return authenticate('admin', 'root')
+    if (err) {
+
+    console.log("error server connect",err)
+  //  throw err;
+}
 })
